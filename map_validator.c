784305c4ee4bc7ca1eea9689fbb446d85d6824c7
Ayman Abourizq <aabouriz@c4r7p9.1337.ma>
2025-02-03 18:36:37 +0100

    LINUX

--- map_validator.c
+++ map_validator.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   map_validator.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aabouriz <aabouriz@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/03 18:36:26 by aabouriz          #+#    #+#             */
+/*   Updated: 2025/02/03 18:36:27 by aabouriz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "get_next_line.h"
 #include "libft/libft.h"
 #include "so_long.h"
@@ -11,12 +23,19 @@ void	map_validator(char *map, t_map_d *map_d)
 {
 	char	*line;
 	int		fd;
+	int		row;
 
+	row = 0;
 	fd = open(map, O_RDONLY);
-	while (line = get_next_line(fd))
+	map_d->rows = 0;
+	line = get_next_line(fd);
+	while (line)
 	{
 		map_d->rows++;
-		map_d->coloms = ft_strlen(line);
+		if (row == 0)
+			map_d->coloms = ft_strlen(line);
 		free (line);
+		line = get_next_line(fd);
+		row++;
 	}
 }
