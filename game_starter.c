b5166119ba817fe09c65d9f3216cb120bc8c872a
DGankutsuou <DGankutsuou@github.com>
2025-02-19 18:19:05 +0100

    blessed

--- game_starter.c
+++ game_starter.c
@@ -6,7 +6,7 @@
 /*   By: blessed <blessed@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/18 14:43:59 by blessed           #+#    #+#             */
-/*   Updated: 2025/02/18 14:44:51 by blessed          ###   ########.fr       */
+/*   Updated: 2025/02/18 14:58:27 by blessed          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,10 @@
 
 void	game_starter(t_map minf)
 {
-	
+	void	*mlx;
+	void	*win;
+
+	mlx = mlx_init();
+	win = mlx_new_window(mlx, );
+	mlx_xpm_file_to_image();
 }
