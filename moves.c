aa0615d6a71b5a439fff9b1906a1e096ea02baae
Ayman Abourizq <aabouriz@c4r7p7.1337.ma>
2025-02-26 11:24:55 +0000

    LINUX

--- moves.c
+++ moves.c
@@ -6,7 +6,7 @@
 /*   By: aabouriz <aabouriz@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/25 21:19:05 by aabouriz          #+#    #+#             */
-/*   Updated: 2025/02/26 09:58:01 by aabouriz         ###   ########.fr       */
+/*   Updated: 2025/02/26 10:35:24 by aabouriz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,14 +18,19 @@ void	up(t_hook *hook)
 	ft_putnbr_fd(hook->counter, 1);
 	ft_putendl_fd("", 1);
 	hook->counter++;
+	hook->minf->grid[hook->minf->p_xy[1]][hook->minf->p_xy[0]] = '0';
+	if (hook->minf->p_xy[0] == hook->minf->e_xy[0] && hook->minf->p_xy[1] == hook->minf->e_xy[1])
+		hook->minf->grid[hook->minf->p_xy[1]][hook->minf->p_xy[0]] = 'E';
 	if (hook->minf->grid[hook->minf->p_xy[1] - 1][hook->minf->p_xy[0]] == 'C')
 		hook->minf->collectees--;
 	else if (hook->minf->grid[hook->minf->p_xy[1] - 1][hook->minf->p_xy[0]] == 'E')
 	{
-		distroyer(hook);
-		exit(0);
+		if (hook->minf->collectees == 0)
+		{
+			distroyer(hook);
+			exit(0);
+		}
 	}
-	hook->minf->grid[hook->minf->p_xy[1]][hook->minf->p_xy[0]] = '0';
 	hook->minf->grid[hook->minf->p_xy[1] - 1][hook->minf->p_xy[0]] = 'P';
 	hook->minf->p_xy[1] -= 1;
 }
@@ -36,14 +41,19 @@ void	right(t_hook *hook)
 	ft_putnbr_fd(hook->counter, 1);
 	ft_putendl_fd("", 1);
 	hook->counter++;
+	hook->minf->grid[hook->minf->p_xy[1]][hook->minf->p_xy[0]] = '0';
+	if (hook->minf->p_xy[0] == hook->minf->e_xy[0] && hook->minf->p_xy[1] == hook->minf->e_xy[1])
+		hook->minf->grid[hook->minf->p_xy[1]][hook->minf->p_xy[0]] = 'E';
 	if (hook->minf->grid[hook->minf->p_xy[1]][hook->minf->p_xy[0] + 1] == 'C')
 		hook->minf->collectees--;
 	else if (hook->minf->grid[hook->minf->p_xy[1]][hook->minf->p_xy[0] + 1] == 'E')
 	{
-		distroyer(hook);
-		exit(0);
+		if (hook->minf->collectees == 0)
+		{
+			distroyer(hook);
+			exit(0);
+		}
 	}
-	hook->minf->grid[hook->minf->p_xy[1]][hook->minf->p_xy[0]] = '0';
 	hook->minf->grid[hook->minf->p_xy[1]][hook->minf->p_xy[0] + 1] = 'P';
 	hook->minf->p_xy[0] += 1;
 }
@@ -54,14 +64,19 @@ void	left(t_hook *hook)
 	ft_putnbr_fd(hook->counter, 1);
 	ft_putendl_fd("", 1);
 	hook->counter++;
+	hook->minf->grid[hook->minf->p_xy[1]][hook->minf->p_xy[0]] = '0';
+	if (hook->minf->p_xy[0] == hook->minf->e_xy[0] && hook->minf->p_xy[1] == hook->minf->e_xy[1])
+		hook->minf->grid[hook->minf->p_xy[1]][hook->minf->p_xy[0]] = 'E';
 	if (hook->minf->grid[hook->minf->p_xy[1]][hook->minf->p_xy[0] - 1] == 'C')
 		hook->minf->collectees--;
 	else if (hook->minf->grid[hook->minf->p_xy[1]][hook->minf->p_xy[0] - 1] == 'E')
 	{
-		distroyer(hook);
-		exit(0);
+		if (hook->minf->collectees == 0)
+		{
+			distroyer(hook);
+			exit(0);
+		}
 	}
-	hook->minf->grid[hook->minf->p_xy[1]][hook->minf->p_xy[0]] = '0';
 	hook->minf->grid[hook->minf->p_xy[1]][hook->minf->p_xy[0] - 1] = 'P';
 	hook->minf->p_xy[0] -= 1;
 }
@@ -71,14 +86,19 @@ void	down(t_hook *hook)
 	ft_putnbr_fd(hook->counter, 1);
 	ft_putendl_fd("", 1);
 	hook->counter++;
+	hook->minf->grid[hook->minf->p_xy[1]][hook->minf->p_xy[0]] = '0';
+	if (hook->minf->p_xy[0] == hook->minf->e_xy[0] && hook->minf->p_xy[1] == hook->minf->e_xy[1])
+		hook->minf->grid[hook->minf->p_xy[1]][hook->minf->p_xy[0]] = 'E';
 	if (hook->minf->grid[hook->minf->p_xy[1] + 1][hook->minf->p_xy[0]] == 'C')
 		hook->minf->collectees--;
 	else if (hook->minf->grid[hook->minf->p_xy[1] + 1][hook->minf->p_xy[0]] == 'E')
 	{
-		distroyer(hook);
-		exit(0);
+		if (hook->minf->collectees == 0)
+		{
+			distroyer(hook);
+			exit(0);
+		}
 	}
-	hook->minf->grid[hook->minf->p_xy[1]][hook->minf->p_xy[0]] = '0';
 	hook->minf->grid[hook->minf->p_xy[1] + 1][hook->minf->p_xy[0]] = 'P';
 	hook->minf->p_xy[1] += 1;
 }
